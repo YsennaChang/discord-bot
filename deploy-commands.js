7ee179fff29b570280208e0a842b7f82a9fd993b
Chang <changcatherinetchin@gmail.com>
2022-10-17 17:32:42 +0200

    correction deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,8 +8,8 @@ const { clientId, guildId } = require('./config.json');
 const commands = [];
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 
-console.log(fs.readdirSync("./commands"));
-const commandFiles = fs.readdirSync("./commands").filter( file => file.endsWith(".js"));
+
+const commandFiles = fs.readdirSync("./commands");
 
 
 for (const file of commandFiles) {
