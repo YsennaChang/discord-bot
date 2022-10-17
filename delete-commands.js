23e40098585416ec016564d71feed39f00824719
Chang <changcatherinetchin@gmail.com>
2022-10-17 16:59:47 +0200

    deleting commands.js

--- delete-commands.js
+++ delete-commands.js
@@ -3,7 +3,12 @@ const { clientId } = require('./config.json');
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-    // for global commands
-rest.put(Routes.applicationCommands(clientId), { body: [] })
-.then(() => console.log('Successfully deleted all application commands.'))
-.catch(console.error);
+// for guild-based commands
+rest.delete(Routes.applicationGuildCommand(clientId, "956319785434619944", '1025679972356063272'))
+	.then(() => console.log('Successfully deleted potes à la compote command'))
+	.catch(console.error);
+
+// for guild-based commands
+rest.delete(Routes.applicationGuildCommand(clientId, "612095258578255893", '1024688175781777528'))
+	.then(() => console.log('Successfully deleted bonsai command'))
+	.catch(console.error);
