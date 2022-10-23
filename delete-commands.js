eb375aa57ee1d70cb3ae903ddd9997c35891d57d
Chang <changcatherinetchin@gmail.com>
2022-10-23 17:33:45 +0200

    modification du chemin d'accès au token

--- delete-commands.js
+++ delete-commands.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const { clientId } = require('./config.json');
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 // for guild-based commands
 rest.delete(Routes.applicationGuildCommand(clientId, "956319785434619944", '1025679972356063272'))
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const {Client, Collection, GatewayIntentBits} = require('discord.js');
-const { token } = require('./config.json');
 const schedule = require("node-schedule");
 const flatCache = require("flat-cache");
 const kv = flatCache.load("cacheId")
@@ -163,5 +162,5 @@ client.on('interactionCreate', async interaction => {
 });
 
 // connecter le script au bot discord
-client.login(token);
+client.login(process.env.TOKEN);
 
